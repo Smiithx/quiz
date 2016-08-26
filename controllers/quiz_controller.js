118a0a4d480aa2692d553b98a44e209574906fc2
Andrés Gonzalez <smiith17@gmail.com>
2016-08-25 21:46:55 -0400

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -21,10 +21,10 @@ exports.show = function(req,res){
 // GET /quizes/answer
 exports.answer = function(req,res){
     var resultado = "Incorrecto";
-    if(req.query.respuesta.toLowerCase() === req.quiz.respuesta){
+    if(req.query.respuesta.toLowerCase() == req.quiz.respuesta.toLowerCase()){
         resultado = "Correcto";
     }
-    res.render("quizes/answer", {Quiz: req.quiz,respuesta : resultado, errors: []});
+    res.render("quizes/answer", {quiz: req.quiz,respuesta : resultado, errors: []});
 
 };
 
@@ -61,4 +61,30 @@ exports.create = function(req,res){
             // res.redirect: Redirección HTTP a lista de preguntas
         }
     });
+};
+
+// GET quizes/:id/edit
+exports.edit = function(req,res){
+    var quiz = req.quiz; // autoload de instancia de quiz
+    
+    res.render("quizes/edit",{quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req,res){
+    req.quiz.pregunta = req.body.quiz.pregunta;   
+    req.quiz.respuesta = req.body.quiz.respuesta;
+    
+    req.quiz.validate().then(
+        function(err){
+            if(err){
+                res.render("quizes/edit", {quiz: req.quiz, errors: err.errors});
+            }else{
+                req.quiz.save({ // save: guarda campos pregunta y respuesta en DB
+                    fields: ["pregunta","respuesta"]
+                })
+                .then(function(){ res.redirect("/quizes");});
+            }
+        }
+    );
 };

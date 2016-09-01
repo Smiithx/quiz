ce95789bb48098b46a5acea1b82f8cda8acc1b9b
Andres Gonzalez <smiith17@gmail.com>
2016-09-01 12:47:19 -0400

    test2

--- models/comment.js
+++ models/comment.js
@@ -3,16 +3,16 @@
 module.exports = function(sequelize,DataTypes){
     return sequelize.define("Comment",{
         texto: {
-              type: DataTypes.STRING,
-              validate: { 
-                  notEmpty: {
-                      msg: "-> Falta Comentario"
-                  }
-              }
-          },
+            type: DataTypes.STRING,
+            validate: { 
+                notEmpty: {
+                    msg: "-> Falta Comentario"
+                }
+            }
+        },
         publicado: {
-              type: DataTypes.BOOLEAN,
-            deafultValue: false
-          }
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
     });
 }
